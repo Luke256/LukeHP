ca7e72bc5ce438e61da041a192a79dd916217102
Luke256 <55425834+Luke256@users.noreply.github.com>
2022-08-24 00:14:47 +0900

    twitter card test 4

--- src/components/seo.js
+++ src/components/seo.js
@@ -51,11 +51,11 @@ function SEO({ title, description, image, lang  }) {
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
       <meta property="og:url" content={seo.url} />
-      <meta property="og:type" content="article" />
+      <meta property="og:type" content="website" />
       <meta property="og:title" content={seo.title} />
       <meta property="og:description" content={seo.description} />
       <meta property="og:image" content={seo.image} />
-      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={twitterUsername} />
       <meta name="twitter:title" content={seo.title} />
       <meta name="twitter:description" content={seo.description} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ const Index = () => {
         <main>
             <SEO title="LukeHome"
                 description="Lukeのホームページです。" 
-                image="https://github.com/Luke256/LukeHP/blob/master/assets/icon-96x96.png" 
+                image="https://luke256.dev/icons/icon-96x96.png" 
                 lang="ja"
             />
             <Layer PageTitle="Home">
