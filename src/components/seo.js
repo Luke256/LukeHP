d62d4575724a4ea97522cc0c251886079927856e
Luke <yuta.k.200628@gmail.com>
2022-12-25 22:40:41 +0900

    modified head

--- src/components/seo.js
+++ src/components/seo.js
@@ -17,7 +17,12 @@ const Seo = ({ title, description, pathname, children }) => {
       <title>{seo.title}</title>
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
+      <meta property="og:url" content={seo.url} />
+      <meta property="og:title" content={seo.title} />
+      <meta property="og:description" content={seo.description} />
+      <meta property="og:image" content={seo.image} />
       <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content={seo.twitterUsername} />
       <meta name="twitter:title" content={seo.title} />
       <meta name="twitter:url" content={seo.url} />
       <meta name="twitter:description" content={seo.description} />
